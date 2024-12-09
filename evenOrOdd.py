9870ecdb5e98f0f7153f6fc08279292616f328be
arumuga-gomathi <r.arumugagomathi@gmail.com>
2024-12-09 19:05:46 +0530

    initial commit

---
+++ evenOrOdd.py
@@ -0,0 +1,11 @@
+#This program is designed to classify numbers as even or odd. A simple implementation of the Collatz Conjecture
+def check_number(num):
+    if num % 2 == 0:
+        result = num // 2
+    else:
+        result = num * 3 + 1
+    return result
+
+num = int(input("enter an integer:"))
+result = check_number(num)
+print("Result:",result)
